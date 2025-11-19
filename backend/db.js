4a76ae1a2ca271a685290954ef091c79829a1079
Lau-1416 <bueno.laura@correounivalle.edu.co>
2025-11-18 20:08:01 -0500

    Se edito el archivo db.js y server.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
+// backend/db.js
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
-export const supabase = createClient (
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 module.exports = { supabase };
